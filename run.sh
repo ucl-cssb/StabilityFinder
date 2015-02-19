66d49f989ffd03121ba5226667d65f17c3cd5a25
mirelio <miriam.leon.12@ucl.ac.uk>
2015-02-19 14:24:18 +0000

    renamed run file

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/code-packages/abc-sysbio-code/:/home/ucbtle1/code-packages/cuda-sim-code/:/home/ucbtle1/code-packages/sympy-0.7.3
+exe=/home/ucbtle1/code-packages/abc-sysbio-code/scripts/run-abc-sysbio
+
+
+python read_input.py; python my_abc.py
