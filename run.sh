8a6e865414a09fc04ffcf42ddb046475e2bc353c
mirelio <miriam.leon.12@ucl.ac.uk>
2015-03-24 17:59:14 +0000

    run on k20 only

--- run.sh
+++ run.sh
@@ -2,5 +2,6 @@
 export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/code-packages/abc-sysbio-code/:/home/ucbtle1/code-packages/cuda-sim-code/:/home/ucbtle1/code-packages/sympy-0.7.3
 exe=/home/ucbtle1/code-packages/abc-sysbio-code/scripts/run-abc-sysbio
 
+export CUDA_DEVICE=5
 
 python read_input.py; python my_abc.py
