592394e0d9720045a271392d31417b4ca8c8d20a
mirelio <miriam.leon.12@ucl.ac.uk>
2015-04-07 20:03:26 +0100

    sample initial conditions as many as specified in input file instead of always 100

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/code-packages/abc-sysbio-code/:/home/ucbtle1/code-packages/cuda-sim-code/:/home/ucbtle1/code-packages/sympy-0.7.3
 exe=/home/ucbtle1/code-packages/abc-sysbio-code/scripts/run-abc-sysbio
 
-export CUDA_DEVICE=5
+export CUDA_DEVICE=6
 
 python read_input.py; python my_abc.py
