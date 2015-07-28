8878c72dd24cd9765add49c2289e9cf9e7423446
mirelio <miriam.leon.12@ucl.ac.uk>
2015-07-28 13:52:51 +0100

    add posterior plotting r script

---
+++ stabilitychecker/plot_posterior.R
@@ -0,0 +1,61 @@
+library(ggplot2)
+library(gridExtra)
+
+plot_posterior_distr <- function(results_path, pop_fold_res_path, limits, names){
+
+  print(limits)
+  print(names)
+  p_values_final = read.table(paste(results_path,"/",pop_fold_res_path,"/Parameter_values_final.txt",sep=''))
+  p_weights_final = read.table(paste(results_path,"/",pop_fold_res_path,"/Parameter_weights_final.txt",sep=''))
+  p_values_final <- subset(p_values_final, select = -p_values_final[,1] )
+  p_values_final$param_weights <- unlist(p_weights_final)
+
+  a=limits[1,]
+  b=limits[2,]
+  pltList <- list()
+  #library(gridExtra)
+  k=0
+  for(i in 1:numb_params)
+    for(j in 1:numb_params){
+      k=k+1
+      print(k)
+      if(i==j){
+        pltList[[k]] <-ggplot(p_values_final, aes_string(x=param_names[i], weight=param_names[ncol(p_values_final)])) + geom_density(fill="grey") + xlim(a[i],b[i])+ ggtitle(param_names[i]) +
+          theme(axis.line=element_blank(),
+                plot.title=element_text(size=8, hjust=0,lineheight=0),
+                axis.text.x=element_text(size=6,angle = 90, vjust=0,hjust=1.2),
+                axis.text.y=element_text(size=6),
+                axis.ticks=element_blank(),
+                axis.title.x=element_blank(),
+                axis.title.y=element_blank(),
+                legend.position="none",
+                panel.grid.minor=element_blank(),
+                plot.background=element_blank(),
+                plot.margin=unit(c(0,0,-0.5,0), "lines"))
+      }else{
+        pltList[[k]] <-ggplot(p_values_final, aes_string(x = param_names[i], y = param_names[j], weight=param_names[ncol(p_values_final)])) + xlim(a[i],b[i])+ ylim(a[j],b[j])+
+          stat_density2d(aes(alpha=..level.., fill=..level.., weight=weights),
+                         size=2, bins=10, geom="polygon") +
+          scale_fill_gradient(low = "yellow", high = "red") +
+          scale_alpha(range = c(0.00, 0.5), guide = FALSE) +
+          geom_density2d(colour="black", bins=10)+
+          theme(axis.line=element_blank(),
+                axis.text.x=element_blank(),
+                axis.text.y=element_blank(),
+                axis.ticks=element_blank(),
+                axis.title.x=element_blank(),
+                axis.title.y=element_blank(),
+                legend.position="none",
+                #panel.background=element_blank(),
+                #panel.border=element_blank(),
+                panel.grid.minor=element_blank(),
+                plot.background=element_blank(),
+                plot.margin=unit(c(0,0,-0.5,0), "lines"))
+      }
+    }
+  g <- do.call("arrangeGrob", c(pltList, list(ncol=ncol(p_values_final)-1)))#,main=textGrob(plot_title, vjust=0.5, gp=gpar(fontsize=18, fontface="bold", fontsize=18)))))
+  ggsave(file=paste(results_path,"/",pop_fold_res_path,"/posterior.pdf",sep=''), g,width=9, height=9, dpi=300)
+}
+
+
+plot_posterior_distr(results_path, pop_fold_res_path, limits, names)
