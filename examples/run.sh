73d98bd4b46d15d404191283ebe076d9a0f20b80
mirelio <miriam.leon.12@ucl.ac.uk>
2015-07-29 22:02:55 +0100

    minor change run file

--- examples/run.sh
+++ examples/run.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/cuda-sim-code
 exe=/home/ucbtle1/work/StabilityChecker/dev/StabilityChecker_stable/StabilityChecker/stabilitychecker
 export CUDA_DEVICE=6
 
-python $exe/read_input.py; python $exe/my_abc.py; #Rscript $exe/plot_posterior.R;
+python $exe/read_input.py; python $exe/my_abc.py; Rscript plot_posterior.R;
