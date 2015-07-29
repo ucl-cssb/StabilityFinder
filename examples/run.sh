0f3c50c14e9011c1949abdc97a1e0aee7080f649
mirelio <miriam.leon.12@ucl.ac.uk>
2015-07-29 10:26:45 +0100

    add sim mode to run file exampels

--- examples/run.sh
+++ examples/run.sh
@@ -2,4 +2,4 @@ export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/.local/lib/python2.7/site-packages/s
 exe=/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
 export CUDA_DEVICE=6
 
-python $exe/read_input.py; python $exe/my_abc.py;
+python $exe/read_input.py; python $exe/my_abc.py; #Rscript $exe/plot_posterior.R
