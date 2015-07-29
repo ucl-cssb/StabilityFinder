ac6cf1bbd99979701e67c791852dd7dba96e73ca
mirelio <miriam.leon.12@ucl.ac.uk>
2015-07-29 19:01:32 +0100

    updates

--- examples/run.sh
+++ examples/run.sh
@@ -1,8 +1,12 @@
 #export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
+<<<<<<< Updated upstream
 #exe=/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
 
 exe=/home/ucbtle1/work/StabilityChecker/dev/StabilityChecker_stable/StabilityChecker/stabilitychecker
 
+=======
+exe=/home/ucbtle1/work/StabilityChecker/dev/StabilityChecker_stable/StabilityChecker/stabilitychecker
+>>>>>>> Stashed changes
 export CUDA_DEVICE=6
 
-python $exe/read_input.py; python $exe/my_abc.py; #Rscript $exe/plot_posterior.R
+python $exe/read_input.py; python $exe/my_abc.py; #Rscript $exe/plot_posterior.R;
