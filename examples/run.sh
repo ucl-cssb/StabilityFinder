4bb484f7df48162f5c1c26438149d602365a11ea
mirelio <miriam.leon.12@ucl.ac.uk>
2015-07-29 18:53:53 +0100

    updates

--- examples/run.sh
+++ examples/run.sh
@@ -1,5 +1,8 @@
-export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
-exe=/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
+#export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
+#exe=/home/ucbtle1/.local/lib/python2.7/site-packages/stabilitychecker
+
+exe=/home/ucbtle1/work/StabilityChecker/dev/StabilityChecker_stable/StabilityChecker/stabilitychecker
+
 export CUDA_DEVICE=6
 
 python $exe/read_input.py; python $exe/my_abc.py; #Rscript $exe/plot_posterior.R
