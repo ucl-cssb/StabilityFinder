a7146cdfb0f6eee07c614b5fcfb5ba1f4da3f9d4
Miriam Leon <Mirelio@users.noreply.github.com>
2016-09-30 17:16:55 +0100

    Update and rename run_deter_high_mean_high_var.sh to run.sh

--- examples/Gardner_switch/Deterministic/run.sh
+++ examples/Gardner_switch/Deterministic/run.sh
@@ -4,4 +4,4 @@ export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/cuda-sim-code
 exe=/home/ucbtle1/work/StabilityChecker/dev/StabilityChecker_stable/export_ICs_matrix/StabilityFinder/stabilityfinder
 
 
-python $exe/my_abc.py -i input_file_deter_high_mean_high_var.xml -o results_deter_high_mean_high_var -l gardner_deter_high_mean_high_var.log;# Rscript plot_posterior.R;
+python $exe/my_abc.py -i input_file.xml -o results_deter_high_mean_high_var -l gardner_deter_high_mean_high_var.log;# Rscript plot_posterior.R;
