1ea560f0ae575470fa3f7f2064bc3fba45c075da
Miriam Leon <Mirelio@users.noreply.github.com>
2016-10-01 18:14:49 +0100

    Delete run.sh

--- examples/Gardner_switch/Deterministic/run.sh
+++
@@ -1,7 +0,0 @@
-export PYTHONPATH=$PYTHONPATH:/home/ucbtle1/cuda-sim-code
-
-
-exe=/home/ucbtle1/work/StabilityChecker/dev/StabilityChecker_stable/export_ICs_matrix/StabilityFinder/stabilityfinder
-
-
-python $exe/my_abc.py -i input_file.xml -o results -l log.log;
